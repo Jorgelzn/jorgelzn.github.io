0e3163dba3fb5a6cfc8704b2453923b44a8793a9
Jorgelzn <jorgelizgo@gmail.com>
2024-01-14 13:06:47 +0100

    animations and refactor

--- src/main.js
+++ src/main.js
@@ -1,116 +1,23 @@
 import './style.css';
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
-import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader';
-import { MTLLoader } from 'three/examples/jsm/loaders/MTLLoader';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 import TWEEN from '@tweenjs/tween.js'
 
-function load_OBJ(model,material){
-    const mtlLoader = new MTLLoader()
-    var container = new THREE.Object3D();
-    mtlLoader.load(
-        material,
-        (materials) => {
-            materials.preload()
-            //console.log(materials)
-            const objLoader = new OBJLoader()
-            objLoader.setMaterials(materials)
-            objLoader.load(
-                model,
-                function ( object ) {
-                    object.traverse(function (child) {
-                        if (child.isMesh) {
-                            child.castShadow = true
-                            child.receiveShadow = true
-                        }
-                    })
-                    container.add(object)
-                }
-                ,
-                (xhr) => {
-                    //console.log((xhr.loaded / xhr.total) * 100 + '% loaded')
-                },
-                (error) => {
-                    console.log('An error happened')
-                }
-                )
-        },
-        (xhr) => {
-            //console.log((xhr.loaded / xhr.total) * 100 + '% loaded')
-        },
-        (error) => {
-            console.log('An error happened')
-        }
-    )
-    return container
-}
-
-function load_GLTF(model){
-    // Instantiate a loader
-    //const loader = new GLTFLoader();
-    var container = new THREE.Object3D();
-
-    // Load a glTF resource
-    gltfLoader.load(
-        // resource URL
-        model,
-        // called when the resource is loaded
-        function ( gltf ) {
-            gltf.scene.traverse(function (child) {
-                if (child.isMesh) {
-                    child.castShadow = true
-                    child.receiveShadow = true
-                }
-            })
-            container.add(gltf.scene);
-        },
-        // called while loading is progressing
-        function ( xhr ) {
-            //console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
-        },
-        // called when loading has errors
-        function ( error ) {
-            console.log( 'An error happened' );
-        }
-    );
-    return container
-}
-
-
-window.onresize = function onWindowResize() {
-    
-    var ratio = document.documentElement.clientWidth/document.documentElement.clientHeight
-    FOV = 75
-    if(document.documentElement.clientWidth<500){
-        FOV = 120;
-    }
-
-	camera.aspect = ratio;
-    camera.fov = FOV
-	camera.updateProjectionMatrix();
-    goHome()
-	renderer.setSize( window.innerWidth, window.innerHeight );
-    controls.update();
-}
-
-
-const raycaster = new THREE.Raycaster()
-const mouse = new THREE.Vector2()
-function raycast(e,object){
-    mouse.set((e.clientX / window.innerWidth) * 2 - 1, -(e.clientY / window.innerHeight) * 2 + 1)
-    raycaster.setFromCamera(mouse, camera)
-    const intersects = raycaster.intersectObjects(scene.children, true)
-    var hit = false
-    object.traverse((element)=>{
-        if(intersects[0].object.uuid == element.uuid){
-            hit=true
-        }
-    })
-    return hit
-}
-
-
+//const raycaster = new THREE.Raycaster()
+//const mouse = new THREE.Vector2()
+//function raycast(e,object){
+//    mouse.set((e.clientX / window.innerWidth) * 2 - 1, -(e.clientY / window.innerHeight) * 2 + 1)
+//    raycaster.setFromCamera(mouse, camera)
+//    const intersects = raycaster.intersectObjects(scene.children, true)
+//    var hit = false
+//    object.traverse((element)=>{
+//        if(intersects[0].object.uuid == element.uuid){
+//            hit=true
+//        }
+//    })
+//    return hit
+//}
 
 function doSmoothReset( ){
     // get current angles
@@ -131,100 +38,96 @@ function doSmoothReset( ){
     }
 }
 
+function load_GLTF(model){
+    // Instantiate a loader
+    //const loader = new GLTFLoader();
+    var container = new THREE.Object3D();
+    var animations = [];
+    // Load a glTF resource
+    gltfLoader.load(
+        // resource URL
+        model,
+        // called when the resource is loaded
+        function ( gltf ) {
+            gltf.scene.traverse(function (child) {
+                if (child.isMesh) {
+                    child.castShadow = true
+                    child.receiveShadow = true
+                }
+            })
+            if(gltf.animations.length>0){
+                var mixer = new THREE.AnimationMixer(gltf.scene);
+                const clips = gltf.animations;
+                clips.forEach( function ( clip ) {
+                    mixer.clipAction( clip ).play();
+                } );
+                animations.push(mixer);
+            }
+            container.add(gltf.scene);
+        },
+        // called while loading is progressing
+        function ( xhr ) {
+            //console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
+        },
+        // called when loading has errors
+        function ( error ) {
+            console.log( 'An error happened' );
+        }
+    );
+    return [container,animations]
+}
+
+
+window.onresize = function onWindowResize() {
+    
+    var ratio = document.documentElement.clientWidth/document.documentElement.clientHeight
+    FOV = 75
+    if(document.documentElement.clientWidth<500){
+        FOV = 120;
+    }
+
+	camera.aspect = ratio;
+    camera.fov = FOV
+	camera.updateProjectionMatrix();
+	renderer.setSize( window.innerWidth, window.innerHeight );
+}
+
 var animLock = false
 window.goLocation = function goLocation(location,name){
-    controls.enableRotate = false;
     if(!animLock && actual_section!=name){
-        animLock=true
-        if(actual_section!="home"){
-            animLock = true
-            document.getElementById(actual_section).style.display = "none"
-            var restore_object = section_objects.find((element)=>{return element.section==actual_section})
-            new TWEEN.Tween(restore_object.position).to({
-                x: restore_object.position.x,
-                y: restore_object.position.y-0.8,
-                z: restore_object.position.z-0.4,
-            },1000).easing(TWEEN.Easing.Cubic.Out).onStart(
-                () => {new TWEEN.Tween(restore_object.rotation).to({
-                    x: restore_object.rotation.x-0.5,
-                    y: restore_object.rotation.y,
-                    z: restore_object.rotation.z,
-                },1000).easing(TWEEN.Easing.Cubic.Out).start()
-                }
-            ).start()
+        if(name=="home"){
+            controls.enableRotate=true
+        }else{
+        controls.enableRotate = false;
         }
+
+        animLock=true
+        try{
+            document.getElementById(actual_section).style.display = "none"
+        }catch{}
+
         actual_section = name
-        var object = section_objects.find((element)=>{return element.section==actual_section})
         const goAnim = new TWEEN.Tween(camera.position).to({
                 x: location[0],
                 y: location[1],
                 z: location[2],
-            },1000).easing(TWEEN.Easing.Cubic.Out).onStart(
+            },2000).easing(TWEEN.Easing.Cubic.Out).onStart(
                 () => {new TWEEN.Tween(camera.rotation).to({
                 x: location[3],
                 y: location[4],
                 z: location[5],
-                },1000).easing(TWEEN.Easing.Cubic.Out).start()
+                },2000).easing(TWEEN.Easing.Cubic.Out).start()
                 }
             )
-
-        const bringObject = new TWEEN.Tween(object.position).to({
-                x: object.position.x,
-                y: object.position.y+0.8,
-                z: object.position.z+0.4,
-            },1000).easing(TWEEN.Easing.Cubic.Out).onStart(
-                () => {new TWEEN.Tween(object.rotation).to({
-                    x: object.rotation.x+0.5,
-                    y: object.rotation.y,
-                    z: object.rotation.z,
-                },1000).easing(TWEEN.Easing.Cubic.Out).start()
-                }
-            )
-        
-        bringObject.onComplete(()=>{
+            goAnim.onComplete(()=>{
             animLock=false
+            try{
             document.getElementById(actual_section).style.display = "flex"
+            }catch{}
         })
-        goAnim.chain(bringObject)
         goAnim.start()
     }
 }
-
-window.goHome = function goHome(){ 
-    if(!animLock && actual_section!="home"){
-        controls.enableRotate=true
-        animLock=true
-        document.getElementById(actual_section).style.display = "none"
-        var restore_object = section_objects.find((element)=>{return element.section==actual_section})
-        new TWEEN.Tween(restore_object.position).to({
-            x: restore_object.position.x,
-            y: restore_object.position.y-0.8,
-            z: restore_object.position.z-0.4,
-        },1000).easing(TWEEN.Easing.Cubic.Out).onStart(
-            () => {new TWEEN.Tween(restore_object.rotation).to({
-                x: restore_object.rotation.x-0.5,
-                y: restore_object.rotation.y,
-                z: restore_object.rotation.z,
-            },1000).easing(TWEEN.Easing.Cubic.Out).start()
-            }
-        ).start()
-        actual_section = "home"
-        new TWEEN.Tween(camera.position).to({
-                x: 6,
-                y: 6,
-                z: 6,
-            },1000).easing(TWEEN.Easing.Cubic.Out).onStart(
-                ()=> {new TWEEN.Tween(camera.rotation).to({
-                    x: -0.785398163397448,
-                    y: 0.6154797086703871,
-                    z: 0.5235987755982985,
-                    },1000).easing(TWEEN.Easing.Cubic.Out).start()
-                    }
-            ).onComplete(()=>{animLock=false}).start()
-    }
-}
-
-
 
 
 // Animation loop
@@ -256,6 +159,10 @@ function animate(){
     if(controls.enableRotate){
         controls.update();
     }
+
+    fish[1].forEach( function ( mixer ) {
+        mixer.update(0.01);
+    });
 
     renderer.render(scene, camera);
 }
@@ -302,13 +209,13 @@ camera.position.setZ(6);
 var section_objects = []
 
 var room_url = require("url:../static/models/room.glb");
-var room = load_GLTF(room_url);
+var room = load_GLTF(room_url)[0];
 room.rotateY(1.5);
 room.position.setY(-3);
 scene.add(room);
 
 var book_url = require("url:../static/models/book.glb");
-var book = load_GLTF(book_url);
+var book = load_GLTF(book_url)[0];
 book.position.setY(-2.2);
 book.position.setX(2.4);
 book.position.setZ(-1.6);
@@ -317,25 +224,26 @@ book.rotateZ(-0.3);
 book.section = "writing"
 section_objects.push(book)
 
-var book_placeholder1 = load_GLTF(book_url);
-book_placeholder1.position.setY(-2.2);
-book_placeholder1.position.setX(2.4);
-book_placeholder1.position.setZ(-1.6);
-book_placeholder1.rotateY(-0.8);
-book_placeholder1.rotateZ(-0.3);
-book_placeholder1.section = "coding"
-section_objects.push(book_placeholder1)
+var fish_url = require("url:../static/models/fish.glb");
+var fish = load_GLTF(fish_url);
+fish[0].position.setY(-1.15);
+fish[0].position.setX(2);
+fish[0].position.setZ(-3.3);
+//fish.rotateY(-0.8);
+//fish.rotateZ(-0.3);
+fish[0].scale.set(0.03,0.03,0.03);
+fish[0].section = "research"
+section_objects.push(fish[0])
 
-var book_placeholder2 = load_GLTF(book_url);
+var book_placeholder2 = load_GLTF(book_url)[0];
 book_placeholder2.position.setY(-2.2);
 book_placeholder2.position.setX(2.4);
 book_placeholder2.position.setZ(-1.6);
 book_placeholder2.rotateY(-0.8);
 book_placeholder2.rotateZ(-0.3);
-book_placeholder2.section = "research"
+book_placeholder2.section = "coding"
 section_objects.push(book_placeholder2)
-scene.add(room,book,book_placeholder1,book_placeholder2);
-//document.addEventListener('mousedown', (e)=>onMouseDown(e,book,10), false);
+scene.add(room,book,fish[0],book_placeholder2);
 
 var background = new THREE.Mesh(
     new THREE.SphereGeometry(50),
@@ -367,12 +275,6 @@ var stars = Array(num_stars).fill().map(()=>{
 
 //LIGHTNING
 
-//const pointLight1 = new THREE.PointLight( 0x03fcfc, 60, 5 );
-//pointLight1.position.set( -5, -1, -3 );
-//pointLight1.castShadow = true;
-//pointLight1.shadow.bias=-0.04;
-//scene.add( pointLight1 )
-
 const directionalLight1 = new THREE.DirectionalLight( 0xffffff, 1 );
 directionalLight1.position.set( 0, 100, -90 );
 
@@ -390,15 +292,6 @@ directionalLight3.position.set( 60, 60, 0 );
 //directionalLight3.shadow.camera.bottom = -d;
 
 scene.add(directionalLight1,directionalLight2,directionalLight3);
-
-//const pointLightHelper1 = new THREE.PointLightHelper( pointLight1 );
-//pointLight1.add(pointLightHelper1);
-//const lightHelper = new THREE.DirectionalLightHelper( directionalLight1 );
-//directionalLight1.add( lightHelper );
-//const lightHelper2 = new THREE.DirectionalLightHelper( directionalLight2 );
-//directionalLight2.add( lightHelper2 );
-//const lightHelper3 = new THREE.DirectionalLightHelper( directionalLight3 );
-//directionalLight3.add( lightHelper3 );
 
 const controls = new OrbitControls( camera, renderer.domElement );
 const original_azimuth = controls.getAzimuthalAngle()
